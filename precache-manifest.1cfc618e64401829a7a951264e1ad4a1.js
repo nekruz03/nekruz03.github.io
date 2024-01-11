886f10f324c2d65cf115856cc8009845413e1d48
Nekruz Nazirjonov <92411934+nekruz03@users.noreply.github.com>
2024-01-11 18:23:03 +0300

    Updates

---
+++ precache-manifest.1cfc618e64401829a7a951264e1ad4a1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ac5333956477971313dcb0d0ea11a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c69aef51eea633bbdd70",
+    "url": "/static/css/2.d470d2ea.chunk.css"
+  },
+  {
+    "revision": "c69aef51eea633bbdd70",
+    "url": "/static/js/2.ba5a8280.chunk.js"
+  },
+  {
+    "revision": "cd1edf37a2b18cbeafde19743c96b7dc",
+    "url": "/static/js/2.ba5a8280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ec155612a122ebf4112",
+    "url": "/static/js/main.82926c1b.chunk.js"
+  },
+  {
+    "revision": "ecbb5a4aebff9c89b516",
+    "url": "/static/js/runtime-main.ff035f97.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "e5ca8c0ac474df46fe45840707a0c483",
+    "url": "/static/media/RobotoMono-Regular.e5ca8c0a.ttf"
+  },
+  {
+    "revision": "21929972ce473acce9de5c1d9661ad93",
+    "url": "/static/media/about-page-img.21929972.png"
+  },
+  {
+    "revision": "09445f5c5e4768486e79eb78fe07f5e0",
+    "url": "/static/media/about-sec-img.09445f5c.png"
+  },
+  {
+    "revision": "501be83e7e45fb894df3e51572a9ef13",
+    "url": "/static/media/cavinimg.501be83e.jpg"
+  },
+  {
+    "revision": "9444f615d5b2976eadd56cd0213e8ca5",
+    "url": "/static/media/cointracker.9444f615.jpg"
+  },
+  {
+    "revision": "8116ad66a0742242fb2b2f284b4053f1",
+    "url": "/static/media/greenctg.8116ad66.jpg"
+  },
+  {
+    "revision": "e1bedcd538ff063127c49a26d0937a68",
+    "url": "/static/media/hero.e1bedcd5.png"
+  },
+  {
+    "revision": "0bf2eabf08e8a9a7bc9e58b107852fcd",
+    "url": "/static/media/map.0bf2eabf.png"
+  },
+  {
+    "revision": "28a260ce758e55a0f3ba35b59edc970d",
+    "url": "/static/media/projectImg.28a260ce.png"
+  },
+  {
+    "revision": "67a7fc34e4f29861d84d88065b198a54",
+    "url": "/static/media/scroll-down-arrow.67a7fc34.svg"
+  },
+  {
+    "revision": "db8e40640abe10c11fbfd65151bf7ff4",
+    "url": "/static/media/social-media-arrow.db8e4064.svg"
+  },
+  {
+    "revision": "3f09455f7f612d6974daf14326a83445",
+    "url": "/static/media/utracker.3f09455f.jpg"
+  }
+]);
